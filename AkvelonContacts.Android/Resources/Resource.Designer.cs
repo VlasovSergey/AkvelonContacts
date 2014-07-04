878dae2505542ab53835edd0901f839e9c396e70
VlasovSergey <qlir@ya.ru>
2014-07-04 18:45:24 +0400

    creates android activity UI

--- AkvelonContacts.Android/Resources/Resource.Designer.cs
+++ AkvelonContacts.Android/Resources/Resource.Designer.cs
@@ -60,6 +60,31 @@ namespace AkvelonContacts.Android
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f060003
+			public const int buttonCall = 2131099651;
+			
+			// aapt resource value: 0x7f060002
+			public const int buttonInfo = 2131099650;
+			
+			// aapt resource value: 0x7f060001
+			public const int buttonsPanel = 2131099649;
+			
+			// aapt resource value: 0x7f060000
+			public const int contactListView = 2131099648;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
