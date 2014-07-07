c83fd8bcc344653a2063bf89e119fd1968b744d6
VlasovSergey <qlir@ya.ru>
2014-07-07 16:58:12 +0400

    adds teamplane for ContactListItem

--- AkvelonContacts.Android/Resources/Resource.Designer.cs
+++ AkvelonContacts.Android/Resources/Resource.Designer.cs
@@ -63,17 +63,20 @@ namespace AkvelonContacts.Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060003
-			public const int buttonCall = 2131099651;
+			// aapt resource value: 0x7f060001
+			public const int buttonCall = 2131099649;
+			
+			// aapt resource value: 0x7f060004
+			public const int buttonInfo = 2131099652;
 			
 			// aapt resource value: 0x7f060002
-			public const int buttonInfo = 2131099650;
+			public const int buttonsPanel = 2131099650;
 			
-			// aapt resource value: 0x7f060001
-			public const int buttonsPanel = 2131099649;
+			// aapt resource value: 0x7f060003
+			public const int contactListView = 2131099651;
 			
 			// aapt resource value: 0x7f060000
-			public const int contactListView = 2131099648;
+			public const int contactName = 2131099648;
 			
 			static Id()
 			{
@@ -89,7 +92,10 @@ namespace AkvelonContacts.Android
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int ContactListViewTemplate = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
