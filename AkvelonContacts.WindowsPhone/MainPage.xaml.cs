361667e993bbcd693e6e445f8b3a5ae4ee506595
VlasovSergey <qlir@ya.ru>
2014-07-11 12:25:53 +0400

    changes UI for windows phone (adds display photo).

--- AkvelonContacts.WindowsPhone/MainPage.xaml.cs
+++ AkvelonContacts.WindowsPhone/MainPage.xaml.cs
@@ -5,6 +5,7 @@
 //-----------------------------------------------------------------------
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
@@ -14,6 +15,7 @@ using System.Windows.Navigation;
 using AkvelonContacts.Common;
 using AkvelonContacts.WindowsPhone.Resources;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Globalization;
 using Microsoft.Phone.Shell;
 
 namespace AkvelonContacts.WindowsPhone
@@ -42,18 +44,6 @@ namespace AkvelonContacts.WindowsPhone
             this.applicationCtrl = new ApplicationController();
 
             this.LoadAndShowContactList();
-            
-            ContactsListBox.SelectionChanged += (object sender, SelectionChangedEventArgs e) =>
-            {
-                if (e.AddedItems != null)
-                {
-                    buttonCall.Visibility = buttonInfo.Visibility = Visibility.Visible;
-                }
-                else
-                {
-                    buttonCall.Visibility = buttonInfo.Visibility = Visibility.Collapsed;
-                }
-            };
         }
         
         /// <summary>
@@ -79,7 +69,7 @@ namespace AkvelonContacts.WindowsPhone
                                     this.contactList = result;
                                 }
 
-                                this.DisplayContactList();
+                                this.OnLoadContactList();
                             });
                         });
                 });
@@ -87,16 +77,108 @@ namespace AkvelonContacts.WindowsPhone
             else
             {
                 this.contactList = this.applicationCtrl.LoadLocalContactsList();
-                this.DisplayContactList();
+                this.OnLoadContactList();
             }
         }
 
         /// <summary>
         /// Displays the contacts list.
         /// </summary>
-        private void DisplayContactList()
+        private void OnLoadContactList()
         {
-            this.ContactsListBox.ItemsSource = this.contactList;
+            List<AlphaKeyGroup<Contact>> dataSource = AlphaKeyGroup<Contact>.CreateGroups(
+                this.contactList,
+                System.Threading.Thread.CurrentThread.CurrentUICulture,
+                (Contact s) => { return s.FullName; },
+                true);
+            ContactsListBox.ItemsSource = dataSource;
+
+            this.applicationCtrl.LoadAllPhotosAsync(this.contactList, (contact) => { });
+        }
+
+        /// <summary>
+        /// Helper class for Grouping.
+        /// </summary>
+        /// <typeparam name="T">Type for grouping.</typeparam>
+        public class AlphaKeyGroup<T> : List<T>
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="AlphaKeyGroup<T>" /> class.
+            /// </summary>
+            /// <param name="key">The key for this group.</param>
+            public AlphaKeyGroup(string key)
+            {
+                this.Key = key;
+            }
+
+            /// <summary>
+            /// The delegate that is used to get the key information.
+            /// </summary>
+            /// <param name="item">An object of type T</param>
+            /// <returns>The key value to use for this object</returns>
+            public delegate string GetKeyDelegate(T item);
+
+            /// <summary>
+            /// Gets the Key of this group.
+            /// </summary>
+            public string Key 
+            {
+                get; 
+                private set; 
+            }
+
+            /// <summary>
+            /// Create a list of AlphaGroup<T> with keys set by a SortedLocaleGrouping.
+            /// </summary>
+            /// <param name="items">The items to place in the groups.</param>
+            /// <param name="ci">The CultureInfo to group and sort by.</param>
+            /// <param name="getKey">A delegate to get the key from an item.</param>
+            /// <param name="sort">Will sort the data if true.</param>
+            /// <returns>An items source for a LongListSelector</returns>
+            public static List<AlphaKeyGroup<T>> CreateGroups(IEnumerable<T> items, CultureInfo ci, GetKeyDelegate getKey, bool sort)
+            {
+                SortedLocaleGrouping slg = new SortedLocaleGrouping();
+                List<AlphaKeyGroup<T>> list = CreateGroups(slg);
+
+                foreach (T item in items)
+                {
+                    int index = 0;
+
+                    index = slg.GetGroupIndex(getKey(item));
+
+                    if (index >= 0 && index < list.Count)
+                    {
+                        list[index].Add(item);
+                    }
+                }
+
+                if (sort)
+                {
+                    foreach (AlphaKeyGroup<T> group in list)
+                    {
+                        group.Sort((c0, c1) => { return ci.CompareInfo.Compare(getKey(c0), getKey(c1)); });
+                    }
+                }
+
+                return list;
+            }
+
+            /// <summary>
+            /// Create a list of AlphaGroup<T> with keys set by a SortedLocaleGrouping.
+            /// </summary>
+            /// <param name="slg">The </param>
+            /// <returns>Theitems source for a LongListSelector</returns>
+            private static List<AlphaKeyGroup<T>> CreateGroups(SortedLocaleGrouping slg)
+            {
+                List<AlphaKeyGroup<T>> list = new List<AlphaKeyGroup<T>>();
+
+                foreach (string key in slg.GroupDisplayNames)
+                {
+                    list.Add(new AlphaKeyGroup<T>(key));
+                }
+
+                return list;
+            }
         }
     }
 }
