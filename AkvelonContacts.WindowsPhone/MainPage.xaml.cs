719a23c915bf41cd7a67e940fd40ed186ada3be5
VlasovSergey <qlir@ya.ru>
2014-07-30 17:55:21 +0400

    moves AlphakeyGroup class to separate class.

--- AkvelonContacts.WindowsPhone/MainPage.xaml.cs
+++ AkvelonContacts.WindowsPhone/MainPage.xaml.cs
@@ -253,7 +253,6 @@ namespace AkvelonContacts.WindowsPhone
         /// <summary>
         /// Hides search panel with focus and shows application bar.
         /// </summary>
-        /// <returns>Is method executed.</returns>
         private void HideSearchTextBox()
         {
             if (searchTextBox.Visibility != Visibility.Visible)
@@ -354,7 +353,7 @@ namespace AkvelonContacts.WindowsPhone
         /// <param name="e">Cancel event args.</param>
         private void PhoneApplicationPage_BackKeyPress(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            if (this.searchTextBox.Visibility == Visibility)
+            if (this.searchTextBox.Visibility == Visibility.Visible)
             {
                 this.HideSearchTextBox();
                 e.Cancel = true;
@@ -379,91 +378,6 @@ namespace AkvelonContacts.WindowsPhone
         private void Grid_MouseLeave(object sender, MouseEventArgs e)
         {
             ((TextBlock)((Grid)sender).FindName("itemNameTextBlock")).Foreground = new SolidColorBrush((App.Current.Resources["PhoneForegroundBrush"] as SolidColorBrush).Color);
-        }
-
-        /// <summary>
-        /// Helper class for Grouping.
-        /// </summary>
-        /// <typeparam name="T">Type for grouping.</typeparam>
-        private class AlphaKeyGroup<T> : List<T>
-        {
-            /// <summary>
-            /// Initializes a new instance of the <see cref="AlphaKeyGroup{T}" /> class.
-            /// </summary>
-            /// <param name="key">The key for this group.</param>
-            public AlphaKeyGroup(string key)
-            {
-                this.Key = key;
-            }
-
-            /// <summary>
-            /// The delegate that is used to get the key information.
-            /// </summary>
-            /// <param name="item">An object of type <typeparamref name="T"/></param>
-            /// <returns>The key value to use for this object</returns>
-            public delegate string GetKeyDelegate(T item);
-
-            /// <summary>
-            /// Gets the Key of this group.
-            /// </summary>
-            public string Key
-            {
-                get;
-                private set;
-            }
-
-            /// <summary>
-            /// Create a list of AlphaGroup <typeparamref name="T"/> with keys set by a SortedLocaleGrouping.
-            /// </summary>
-            /// <param name="items">The items to place in the groups.</param>
-            /// <param name="ci">The CultureInfo to group and sort by.</param>
-            /// <param name="getKey">A delegate to get the key from an item.</param>
-            /// <param name="sort">Will sort the data if true.</param>
-            /// <returns>An items source for a LongListSelector</returns>
-            public static List<AlphaKeyGroup<T>> CreateGroups(IEnumerable<T> items, CultureInfo ci, GetKeyDelegate getKey, bool sort)
-            {
-                SortedLocaleGrouping slg = new SortedLocaleGrouping(ci);
-                List<AlphaKeyGroup<T>> list = CreateGroups(slg);
-
-                foreach (T item in items)
-                {
-                    int index = 0;
-
-                    index = slg.GetGroupIndex(getKey(item));
-
-                    if (index >= 0 && index < list.Count)
-                    {
-                        list[index].Add(item);
-                    }
-                }
-
-                if (sort)
-                {
-                    foreach (AlphaKeyGroup<T> group in list)
-                    {
-                        group.Sort((c0, c1) => { return ci.CompareInfo.Compare(getKey(c0), getKey(c1)); });
-                    }
-                }
-
-                return list;
-            }
-
-            /// <summary>
-            /// Create a list of AlphaGroup <typeparamref name="T"/> with keys set by a SortedLocaleGrouping.
-            /// </summary>
-            /// <param name="slg">The sorted locale grouping.</param>
-            /// <returns>The items source for a LongListSelector.</returns>
-            private static List<AlphaKeyGroup<T>> CreateGroups(SortedLocaleGrouping slg)
-            {
-                List<AlphaKeyGroup<T>> list = new List<AlphaKeyGroup<T>>();
-
-                foreach (string key in slg.GroupDisplayNames)
-                {
-                    list.Add(new AlphaKeyGroup<T>(key));
-                }
-
-                return list;
-            }
         }
     }
 }
