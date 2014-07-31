0315e3821ea601a34b590d9d76fd1c68b416c548
VlasovSergey <qlir@ya.ru>
2014-07-31 17:39:02 +0400

    iOS.adds UI methods.

--- AkvelonContacts.iOS/AkvelonContactsViewController.cs
+++ AkvelonContacts.iOS/AkvelonContactsViewController.cs
@@ -21,6 +21,16 @@ namespace AkvelonContacts.iOS
         private ApplicationController applicationCtrl;
 
         /// <summary>
+        /// Value indicating whether display contacts only with key.
+        /// </summary>
+        private bool displayOnlyContactsWithKey;
+
+        /// <summary>
+        /// Contains contact list.
+        /// </summary>
+        private List<Contact> contactList;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="AkvelonContactsViewController"/> class.
         /// </summary>
         /// <param name="handle">To be added.</param>
@@ -44,9 +54,19 @@ namespace AkvelonContacts.iOS
         {
             base.ViewDidLoad();
 
+            this.displayOnlyContactsWithKey = false;
+
             this.applicationCtrl = new ApplicationController();
 
             this.LoadContactsAndDisplay();
+        }
+
+        /// <summary>
+        /// Called every time any photo loaded.
+        /// </summary>
+        /// <param name="c">Contact which downloaded photo.</param>
+        private void OnLoadPhoto(Contact c)
+        {
         }
 
         /// <summary>
@@ -75,7 +95,9 @@ namespace AkvelonContacts.iOS
             {
                 if (contactList != null)
                 {
-                    ContactsTableView.Source = new TableSource(contactList);
+                    this.contactList = contactList;
+                    DisplayContactList(this.contactList, null);
+                    this.DisplayTimeUpdate();
                 }
                 else
                 {
@@ -84,11 +106,86 @@ namespace AkvelonContacts.iOS
         }
 
         /// <summary>
-        /// Called every time any photo loaded.
+        /// Shows progress indicator.
         /// </summary>
-        /// <param name="c">Contact which downloaded photo.</param>
-        private void OnLoadPhoto(Contact c)
+        private void ShowProgressIndicator()
         {
+        }
+
+        /// <summary>
+        /// Hides progress indicator.
+        /// </summary>
+        private void HideProgressIndicator()
+        {
+        }
+
+        /// <summary>
+        /// Displays the time of last update.
+        /// </summary>
+        private void DisplayTimeUpdate()
+        {
+            DateTimeOffset? updateTimeOrNull = this.applicationCtrl.GetTimeOfLastUpdate();
+
+            if (updateTimeOrNull == null)
+            {
+                return;
+            }
+
+            // updateTimeTextBlock.Text = TimeConverter.GetElapsedTime((DateTimeOffset)updateTimeOrNull);
+        }
+
+        /// <summary>
+        /// Displays contact list.
+        /// </summary>
+        /// <param name="contactList">Contact list for display.</param>
+        /// <param name="contactFilter">Filter for contacts.</param>
+        private void DisplayContactList(List<Contact> contactList, Func<Contact, bool> contactFilter)
+        {
+            var newList = ContactsFilter.FilterContacts(contactList, this.displayOnlyContactsWithKey, contactFilter);
+
+            this.ContactsTableView.Source = new TableSource(newList);            
+        }
+
+        /// <summary>
+        /// Finds and displays contacts by text.
+        /// </summary>
+        /// <param name="searchText">Text for searching.</param>
+        private void FindAndDisplayContactByText(string searchText)
+        {
+            this.DisplayContactList(
+                this.contactList,
+                (Contact contact) =>
+                {
+                    bool mailCriterion = contact.Mail != null && contact.Mail.IndexOf(searchText, System.StringComparison.OrdinalIgnoreCase) >= 0;
+                    bool fullNameCriterion = contact.FullName.IndexOf(searchText, System.StringComparison.OrdinalIgnoreCase) >= 0;
+                    return mailCriterion || fullNameCriterion;
+                });
+        }
+
+        /// <summary>
+        /// Called when the button is clicked, search.
+        /// </summary>
+        /// <param name="sender">Is a parameter called event sender.</param>
+        /// <param name="e">Cancel event args.</param>
+        private void Search_Click(object sender, EventArgs e)
+        {
+            if (this.contactList == null)
+            {
+                // MessageBox.Show(AppResources.MessageIfSearchWithoutContacts, AppResources.WarningTitle, MessageBoxButton.OK);
+            }
+
+            // this.ShowSearchTextBox();
+        }
+
+        /// <summary>
+        /// Called when the button is clicked, refresh.
+        /// </summary>
+        /// <param name="sender">Is a parameter called event sender.</param>
+        /// <param name="e">Cancel event args.</param>
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            this.ShowProgressIndicator();
+            this.UpdateContactsFromServer();
         }
     }
 }
